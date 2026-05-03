73b1c59663f9896eef3ed0fe1e131c6da1c1f0b6
Arthur Noreika <arthurnoreika11@live.com>
2026-05-03 11:05:07 +0100

    Implement hotkey functionality and improve window style for ColourFilterForm

--- WinForms/winForms.cs
+++ WinForms/winForms.cs
@@ -1,11 +1,33 @@
 using System;
 using System.Drawing;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace WinForms
 {
     public class ColourFilterForm : Form
     {
+        private const int GwlExStyle = -20;
+        private const int WsExTransparent = 0x20;
+        private const int WsExToolWindow = 0x80;
+        private const int WsExLayered = 0x80000;
+        private const int HotkeyId = 1;
+        private const int WmHotkey = 0x0312;
+        private const int ModAlt = 0x0001;
+        private const int ModControl = 0x0002;
+
+        [DllImport("user32.dll")]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, Keys vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
         public ColourFilterForm()
         {
             BackColor = Color.FromArgb(255, 242, 168);
@@ -15,17 +37,33 @@ namespace WinForms
             WindowState = FormWindowState.Maximized;
             TopMost = true;
             ShowInTaskbar = false;
-            KeyPreview = true;
-
-            KeyDown += ColourFilterForm_KeyDown;
         }
 
-        private void ColourFilterForm_KeyDown(object sender, KeyEventArgs e)
+        protected override void OnShown(EventArgs e)
         {
-            if (e.KeyCode == Keys.Escape)
+            base.OnShown(e);
+
+            int style = GetWindowLong(Handle, GwlExStyle);
+            style = style | WsExLayered | WsExTransparent | WsExToolWindow;
+            SetWindowLong(Handle, GwlExStyle, style);
+
+            RegisterHotKey(Handle, HotkeyId, ModControl | ModAlt, Keys.Q);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WmHotkey && m.WParam.ToInt32() == HotkeyId)
             {
                 Close();
             }
+
+            base.WndProc(ref m);
+        }
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            UnregisterHotKey(Handle, HotkeyId);
+            base.OnFormClosed(e);
         }
     }
 
